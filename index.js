0c7473bfad34412f9fe7c883021f29801867421e
alex <alex@rocketguys.com>
2019-10-09 21:58:12 +0300

    fix mergeShallowById. ava → jest

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ var cmpVal = function (a, b) { return a !== undefined && a === b; };
  * @returns {?Number} index
  */
 var findIndex =
-function findIndex (arr, filter) {
-  if ('findIndex' in arr) {
-    var index = arr.findIndex(filter);
-    return index !== -1 ? index : undefined;
-  }
+  function findIndex(arr, filter) {
+    if ('findIndex' in arr) {
+      var index = arr.findIndex(filter);
+      return index !== -1 ? index : undefined;
+    }
 
-  var key = undefined;
-  arr.forEach(function (val, key_) { return key === undefined && filter(val, key) && (key = key_); });
-  return key;
-}
+    var key = undefined;
+    arr.forEach(function (val, key_) { return key === undefined && filter(val, key) && (key = key_); });
+    return key;
+  }
 
 
 /**
@@ -30,31 +30,33 @@ function findIndex (arr, filter) {
  * @example [{id:1}, {id:2, b:0}] + [{id:2, c:-1}, {id:3}] == [{id:1}, {id:2, b:0, c:-1}, {id:3}]
  */
 exports.mergeCollectionsBy =
-module.exports =
-exports.default =
-function mergeCollectionsBy (key, opts) {
-  var merge = opts && opts.merge || mergeDeep;
-  var is = opts && opts.is || cmpVal;
-  var shouldPutNewToBottom = opts && opts.shouldPutNewToBottom || true;
+  module.exports =
+  exports.default =
+  function mergeCollectionsBy(key, opts) {
+    var merge = opts && opts.merge || mergeDeep;
+    var is = opts && opts.is || cmpVal;
+    var shouldPutNewToBottom = opts && opts.shouldPutNewToBottom !== undefined
+      ? opts.shouldPutNewToBottom
+      : true;
 
-  /**
-   * @param {Collection} c1
-   * @param {Collection} c2
-   * @returns {Collection}
-   */
-  return function mergeCollectionsByKey (c1, c2) {
-    var c2VisitedIdx = {} // e.g. 42: true
-    var updatedC1 = c1.map(function (item1) {
-      var index2 = findIndex(c2, function (item2) { return is(item2[key], item1[key]); });
-      c2VisitedIdx[index2] = true;
-      return index2 !== undefined
-        ? merge(item1, c2[index2])
-        : item1;
-    });
-    var c2Rest = c2.filter(function (_, key2) { return !c2VisitedIdx[key2]; })
-    return shouldPutNewToBottom ? updatedC1.concat(c2Rest) : c2Rest.concat(updatedC1);
-  }
-};
+    /**
+     * @param {Collection} c1
+     * @param {Collection} c2
+     * @returns {Collection}
+     */
+    return function mergeCollectionsByKey(c1, c2) {
+      var c2VisitedIdx = {} // e.g. 42: true
+      var updatedC1 = c1.map(function (item1) {
+        var index2 = findIndex(c2, function (item2) { return is(item2[key], item1[key]); });
+        c2VisitedIdx[index2] = true;
+        return index2 !== undefined
+          ? merge(item1, c2[index2])
+          : item1;
+      });
+      var c2Rest = c2.filter(function (_, key2) { return !c2VisitedIdx[key2]; })
+      return shouldPutNewToBottom ? updatedC1.concat(c2Rest) : c2Rest.concat(updatedC1);
+    }
+  };
 
 /**
  * @typedef {Array<Object>} Collection
@@ -65,14 +67,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 module.exports.findIndex =
-exports.findIndex =
-findIndex;
+  exports.findIndex =
+  findIndex;
 
 
 module.exports.mergeShallow =
-exports.mergeShallow =
-function (a, b) { return Object.assign({}, a, b); };
+  exports.mergeShallow =
+  function (a, b) { return Object.assign({}, a, b); };
 
 module.exports.mergeDeep =
-exports.mergeDeep =
-mergeDeep;
+  exports.mergeDeep =
+  mergeDeep;
---
+++ index.spec.js
@@ -0,0 +1,113 @@
+const mergeCollectionsBy = require('.');
+const { findIndex, mergeShallow } = require('.');
+
+const mergeById = mergeCollectionsBy('id');
+const mergeByIdTop = mergeCollectionsBy('id', { shouldPutNewToBottom: false });
+const mergeByIdBot = mergeCollectionsBy('id', { shouldPutNewToBottom: true });
+
+test('require is ok', () => {
+  const mergeCollectionsBy_ = require('.');
+  expect(typeof mergeCollectionsBy_).toBe('function');
+});
+
+test('create specialized instance', () => {
+  expect(typeof mergeCollectionsBy('a')).toBe('function');
+});
+
+test('findIndex', () => {
+  expect(findIndex([{ id: 2, c: -1 }, { id: 3 }], item2 => item2['id'] === 2)).toBe(0);
+});
+
+//
+//
+//
+
+test('merge lists by key', () => {
+  const list1 = [{ id: 1 }, { id: 2, b: 0 }];
+  const list2 = [{ id: 2, c: -1 }, { id: 3 }];
+  const merged = [{ id: 1 }, { id: 2, b: 0, c: -1 }, { id: 3 }];
+
+  expect(mergeById(list1, list2)).toEqual(merged);
+});
+
+test('merge lists deeply', () => {
+  const list1 = [{ id: 1, b: { c: 1 } }, { id: 2, b: { d: 2 } }];
+  const list2 = [{ id: 1, b: { c: 3 } }, { id: 2, b: { c: 4 } }];
+  const merged = [{ id: 1, b: { c: 3 } }, { id: 2, b: { c: 4, d: 2 } }];
+
+  expect(mergeById(list1, list2)).toEqual(merged);
+});
+
+test('deep merge object-items', () => {
+  const list1 = [{ id: 1, b: { c: 1 } }, { id: 2, b: { d: 2 } }];
+  const list2 = [{ id: 1, b: { c: 3 } }, { id: 2, b: { c: 4 } }];
+  const merged = [{ id: 1, b: { c: 3 } }, { id: 2, b: { c: 4, d: 2 } }];
+
+  expect(mergeById(list1, list2)).toEqual(merged);
+});
+
+test('no so cool merge deep nested arrays', () => {
+  const list1 = [{ id: 1, b: [1] }, { id: 2, b: [2] }];
+  const list2 = [{ id: 1, b: [3] }, { id: 2, b: [4] }];
+  const merged = [{ id: 1, b: [3] }, { id: 2, b: [4] }];
+
+  expect(mergeById(list1, list2)).toEqual(merged);
+});
+
+test('merge item with a collection', () => {
+  const list = [{ id: 1, b: 2 }, { id: 2, d: 4 }];
+  const item = { id: 1, c: 3 };
+  const merged = [{ id: 1, b: 2, c: 3 }, { id: 2, d: 4 }]
+
+  expect(mergeById(list, [item])).toEqual(merged);
+});
+
+test('add new item into collection', () => {
+  const list = [{ id: 2, b: 2 }, { id: 3, d: 4 }];
+  const item = { id: 1, c: 3 };
+  const merged = [{ id: 2, b: 2 }, { id: 3, d: 4 }, { id: 1, c: 3 }];
+
+  expect(mergeById(list, [item])).toEqual(merged);
+});
+
+test('join collections with no intersected keys', () => {
+  const list1 = [{ id: 1, b: 11 }, { id: 2, b: 12 }];
+  const list2 = [{ id: 3, b: 13 }, { id: 4, b: 14 }];
+  const merged = [{ id: 1, b: 11 }, { id: 2, b: 12 }, { id: 3, b: 13 }, { id: 4, b: 14 }];
+
+  expect(mergeById(list1, list2)).toEqual(merged);
+});
+
+test('merge item with less deep properties', () => {
+  const list1 = [{ id: 1, b: { c: 1 } }];
+  const list2 = [{ id: 1 }];
+  const merged = [{ id: 1, b: { c: 1 } }];
+
+  expect(mergeById(list1, list2)).toEqual(merged);
+});
+
+//
+//
+//
+
+test('shallow merger', () => {
+  const mergeShallowById = mergeCollectionsBy('id', { merge: mergeShallow });
+
+  const list1 = [{ id: 1, b: { c: 1 } }, { id: 2, b: { d: 2 } }];
+  const list2 = [{ id: 1, b: { c: 3 } }, { id: 2, b: { c: 4 } }];
+  const merged = [{ id: 1, b: { c: 3 } }, { id: 2, b: { c: 4 } }];
+  const deep = [{ id: 1, b: { c: 3 } }, { id: 2, b: { c: 4, d: 2 } }];
+
+  expect(mergeShallowById(list1, list2)).toEqual(merged);
+  expect(mergeById(list1, list2)).toEqual(deep);
+});
+
+test('shouldPutNewToBottom', () => {
+  const list1 = [{ id: 2, b: { c: 1 } }];
+  const top = [{ id: 1 }, { id: 2, b: { c: 1 } }];
+  const bot = [{ id: 2, b: { c: 1 } }, { id: 1 }];
+
+  expect(mergeByIdTop(list1, [{ id: 1 }])).toEqual(top);
+  expect(mergeByIdBot(list1, [{ id: 1 }])).toEqual(bot);
+  expect(mergeById(list1, [{ id: 1 }])).toEqual(bot);
+});
